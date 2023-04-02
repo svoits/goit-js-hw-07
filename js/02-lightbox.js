33bbad5771ba858a67b22d9f15417c4c3c433ab4
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-04-02 11:16:21 +0200

    solved task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// select gallery container
+const galleryList = document.querySelector('.gallery');
+
+// create markup of gallery items
+galleryList.innerHTML = markup(galleryItems);
+
+// creates markup of gallery items
+function markup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+                src="${preview}"
+                alt="${description}" />
+            </a>
+        </li>
+    `;
+    })
+    .join('');
+}
+
+// add lightbox to the gallery items
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
