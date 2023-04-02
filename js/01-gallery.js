3a7265407b6c47c553d7bf977fd3afdb6947c1ca
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-04-02 11:16:16 +0200

    solved task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// select gallery container
+const galleryList = document.querySelector('.gallery');
+
+// create markup of gallery items
+galleryList.innerHTML = markup(galleryItems);
+
+// add listener to the gallery container / images - delegate
+galleryList.addEventListener('click', onImagePreviewClick);
+
+// creates markup of gallery items
+function markup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+    `;
+    })
+    .join('');
+}
+
+// creates modal image window and closes it, if image is opened
+function onImagePreviewClick(e) {
+  e.preventDefault();
+  const { target } = e;
+
+  if (!target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`<img src="${target.dataset.source}"/>`, {
+    onShow: () => window.addEventListener('keydown', onEscButtonClick),
+    onClose: () => window.removeEventListener('keydown', onEscButtonClick),
+  });
+
+  instance.show();
+
+  function onEscButtonClick(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
